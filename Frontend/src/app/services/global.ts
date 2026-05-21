25f8ec9053b721ca5896a8e50d9164bbfc6a4703
Adrian Santana <santana.txiki@gmail.com>
2026-05-21 09:07:03 +0200

    Limpia referencias antiguas y ajusta integracion meteorologica

--- Frontend/src/app/services/global.ts
+++
@@ -1,6 +0,0 @@
-export const GLOBAL = {
-  API_URL:'https://pokeapi.co/api/v2/pokemon/',
-  IMAGEN_URL:'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/',
-  ESPECIES_URL:'https://pokeapi.co/api/v2/pokemon-species/',
-  EVOLUCION_URL:'https://pokeapi.co/api/v2/evolution-chain/'
-}
